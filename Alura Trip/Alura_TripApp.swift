dee5ea5c2a26d93bf9d4a5d401ff4aa7bfa32c07
Joao Magioli <joaomagioli@gmail.com>
2021-08-11 22:16:43 -0300

    Initial Commit

---
+++ Alura Trip/Alura_TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alura_TripApp.swift
+//  Alura Trip
+//
+//  Created by João Guilherme Magioli de Souza on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Alura_TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
